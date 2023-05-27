87c3e25239045bbe7d2b1a7a0487bc2a38399ede
Hikmat Samadov <s3m3dov@hotmail.com>
2023-05-27 16:45:17 +0400

    refactor: optimize imports

--- core/database.py
+++ core/database.py
@@ -1,11 +1,5 @@
 from sqlalchemy import create_engine
 
 from core.config import settings
-from core.entities.models import (
-    PromptMessage,
-    OutputMessage,
-    User,
-    UserChannel,
-)
 
 engine = create_engine(settings.database_url, echo=True)

e86b2a73b7fa1deaf97a51c0199897d1ddfcf2b3
Hikmat Samadov <s3m3dov@hotmail.com>
2023-05-27 14:08:26 +0400

    refactor: rename platform_chat_id to platform_user_id

--- app/tg/__init__.py
+++ app/tg/__init__.py
@@ -7,6 +7,7 @@ from app.tg.commands import (
     echo,
     ask_gpt3_5_turbo,
     ask_gpt4,
+    subscribe
 
 )
 
@@ -16,6 +17,7 @@ command_handlers = [
     CommandHandler("echo", echo),
     CommandHandler("gpt3_5_turbo", ask_gpt3_5_turbo),
     CommandHandler("gpt4", ask_gpt4),
+    CommandHandler("subscribe", subscribe),
 ]
 command_info = [
     BotCommand("start", "Start the bot"),
@@ -23,4 +25,5 @@ command_info = [
     BotCommand("echo", "Echo the message"),
     BotCommand("gpt3_5_turbo", "Ask GPT-3.5 Turbo a question"),
     BotCommand("gpt4", "Ask GPT-4 a question"),
+    BotCommand("subscribe", "Subscribe to the premium plan"),
 ]
--- app/tg/commands.py
+++ app/tg/commands.py
@@ -1,9 +1,10 @@
 from sqlalchemy.sql import select, insert
-from telegram import Update
+from telegram import Update, LabeledPrice
 from telegram.constants import ChatType
 from telegram.ext import ContextTypes
 
 from app.ai.llms import gpt3_5_turbo, gpt4
+from app.core.config import config, settings
 from app.db import engine
 from app.db.enums import Platform, SystemUser
 from app.db.models import User, UserChannel
@@ -18,7 +19,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     platform_user_id = tg_user.id
 
     if tg_chat.type != ChatType.PRIVATE.value:
-        await update.message.reply_text("It's only possible to use this bot in private chats.")
+        await update.message.reply_text(
+            "It's only possible to use this bot in private chats."
+        )
         return
 
     with engine.connect() as conn:
@@ -45,14 +48,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             )
             result = conn.execute(statement_2)
             user_id = result.first()[0]
-            statement_3 = (
-                insert(UserChannel)
-                .values(
-                    platform=Platform.TELEGRAM,
-                    platform_user_id=tg_chat.id,
-                    user_id=user_id,
-                    data=tg_chat.dict(),
-                )
+            statement_3 = insert(UserChannel).values(
+                platform=Platform.TELEGRAM,
+                platform_user_id=tg_chat.id,
+                user_id=user_id,
+                data=tg_chat.dict(),
             )
             result = conn.execute(statement_3)
             print(result)
@@ -60,7 +60,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -70,7 +70,9 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def ask_gpt3_5_turbo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not context.args:
-        await update.message.reply_text("You need to ask a question in the format `/gpt3_5_turbo <question>`")
+        await update.message.reply_text(
+            "You need to ask a question in the format `/gpt3_5_turbo <question>`"
+        )
         return
 
     platform_user_id = update.effective_user.id
@@ -78,13 +80,19 @@ async def ask_gpt3_5_turbo(update: Update, context: ContextTypes.DEFAULT_TYPE) -
     question = " ".join(context.args)
     response = ask_chat_openai(llm=gpt3_5_turbo, question=question)
     await update.message.reply_text(response)
-    save_prompt_n_output_to_db(platform_user_id=platform_user_id, prompt=question, output=response,
-                               system_sender=SystemUser.GPT_3_5_TURBO)
+    save_prompt_n_output_to_db(
+        platform_user_id=platform_user_id,
+        prompt=question,
+        output=response,
+        system_sender=SystemUser.GPT_3_5_TURBO,
+    )
 
 
 async def ask_gpt4(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not context.args:
-        await update.message.reply_text("You need to ask a question in the format `/gpt4 <question>`")
+        await update.message.reply_text(
+            "You need to ask a question in the format `/gpt4 <question>`"
+        )
         return
 
     platform_user_id = update.effective_user.id
@@ -92,10 +100,13 @@ async def ask_gpt4(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     question = " ".join(context.args)
     response = ask_chat_openai(llm=gpt4, question=question)
     await update.message.reply_text(response)
-    save_prompt_n_output_to_db(platform_user_id=platform_user_id, prompt=question, output=response,
-                               system_sender=SystemUser.GPT_4)
+    save_prompt_n_output_to_db(
+        platform_user_id=platform_user_id,
+        prompt=question,
+        output=response,
+        system_sender=SystemUser.GPT_4,
+    )
 
 
 async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    platform_user_id = update.effective_user.id
-    await update.message.reply_text("You have been subscribed to the newsletter!")
+    pass

6c3c7ddac60f82109e32e6eb64eedd619c36de63
Hikmat Samadov <s3m3dov@hotmail.com>
2023-06-04 00:20:28 +0400

    refactor: update settings

--- config/main.py
+++ config/main.py
@@ -1,6 +1,8 @@
 from dotenv import dotenv_values
-
 from pydantic import BaseSettings
+
+from entities.config_schemas.main import LLMConfig, TelegramConfig, DBConfig, StripeConfig
+from entities.enums import SystemUser
 
 config = dotenv_values(".env")
 
@@ -8,47 +10,37 @@ config = dotenv_values(".env")
 class Config(BaseSettings):
     """Config class for settings."""
 
-    max_tokens = {
-        "default": 300,
-        "gpt-3": 300,
-        "gpt-3.5": 300,
-        "gpt-3.5-turbo": 300,
-        "gpt-4": 300,
-        "dall-e": 300,
-    }
-    temperature = {
-        "default": 0.3,
-        "gpt-3": 0.3,
-        "gpt-3.5": 0.3,
-        "gpt-3.5-turbo": 0.3,
-        "gpt-4": 0.3,
-        "dall-e": 0.3,
-    }
-    payment_link = config["STRIPE_PAYMENT_LINK"]
-    stripe_public_key = config["STRIPE_API_PUBLISHABLE_KEY"]
-    stripe_secret_key = config["STRIPE_API_SECRET_KEY"]
-    stripe_webhook_secret = config["STRIPE_WEBHOOK_SECRET"]
-    stripe_price_id = "price_1NAWLbHCGQv2kDWZu8N1uuOO"
-    stripe_success_url = "https://s3m3dov.github.io/simple-success-page/"
-    currency = "usd"
-    phone_number_collection_enabled = True
-    trial_period_days = 2
-    dashboard_url = "https://billing.stripe.com/p/login/test_14kfZT5bre7ubO8fYY"
-
-    @property
-    def telegram_bot_token(self) -> str:
-        """Telegram bot token."""
-        return config["TELEGRAM_BOT_TOKEN"]
-
-    @property
-    def database_url(self) -> str:
-        """Database url."""
-        url = "{driver}://{user}:{password}@{host}:{port}/{database}".format(
-            driver=config["DB_DRIVER"],
-            user=config["DB_USER"],
-            password=config["DB_PASSWORD"],
-            host=config["DB_HOST"],
-            port=config["DB_PORT"],
-            database=config["DB_DATABASE"],
-        )
-        return url
+    telegram: TelegramConfig = TelegramConfig(
+        bot_token=config["TELEGRAM_BOT_TOKEN"],
+    )
+    db: DBConfig = DBConfig(
+        driver=config["DB_DRIVER"],
+        user=config["DB_USER"],
+        password=config["DB_PASSWORD"],
+        host=config["DB_HOST"],
+        port=config["DB_PORT"],
+        database=config["DB_DATABASE"],
+    )
+    gpt4: LLMConfig = LLMConfig(
+        name=SystemUser.GPT_4,
+        temperature=0.3,
+        max_tokens=300,
+        free_tokens=2000,
+        paid_tokens_per_day=10000,
+    )
+    gpt3_5_turbo: LLMConfig = LLMConfig(
+        name=SystemUser.GPT_3_5_TURBO,
+        temperature=0.3,
+        max_tokens=300,
+        free_tokens=2000,
+        paid_tokens_per_day=10000,
+    )
+    stripe: StripeConfig = StripeConfig(
+        api_pub_key=config["STRIPE_API_PUBLISHABLE_KEY"],
+        api_secret_key=config["STRIPE_API_SECRET_KEY"],
+        webhook_secret=config["STRIPE_WEBHOOK_SECRET"],
+        success_url="https://s3m3dov.github.io/simple-success-page/",
+        dashboard_url="https://billing.stripe.com/p/login/test_14kfZT5bre7ubO8fYY",
+        price_id="price_1NAWLbHCGQv2kDWZu8N1uuOO",
+        trial_period_days=2,
+    )

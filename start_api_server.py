9d3edc610de04d2baac7f811cb581ea3b7be63d6
Hikmat Samadov <s3m3dov@hotmail.com>
2023-06-03 20:01:48 +0400

    refactor: update start scripts

--- start_api_server.py
+++ start_api_server.py
@@ -1,6 +1,8 @@
 import uvicorn
 
-from app.api.app import app  # noqa: F401 (representing for command below)
+from app_api.app import app  # noqa: F401 (representing for command below)
 
 if __name__ == "__main__":
-    uvicorn.run(app="app.api.app:app", host="localhost", port=8000, reload=True)
+    uvicorn.run(
+        app="app_api.app:app", host="localhost", port=8000, reload=True
+    )

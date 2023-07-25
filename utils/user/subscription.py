6e16424ed4d270017552ade2e51e0e0d02881435
Hikmat Samadov <s3m3dov@hotmail.com>
2023-07-25 10:34:58 +0400

    chore: update queries

--- utils/user/subscription.py
+++ utils/user/subscription.py
@@ -1,6 +1,7 @@
 import stripe
 
 from core.config import settings
+from core.database import session_scope
 from core.logging import logger
 from entities.models import (
     User,
@@ -16,34 +17,43 @@ stripe.api_key = settings.stripe.api_secret_key
 class SubscriptionCRUD:
     @staticmethod
     def create_subscription(subscription: SubscriptionSchema):
-        user = User.where(customer_id__exact=subscription.customer).first()
-        if not user:
-            logger.error(f"User not found: {subscription.customer}")
-            return
+        with session_scope() as session:
+            user = (
+                session.query(User)
+                .filter(User.customer_id == subscription.customer)
+                .first()
+            )
+            if not user:
+                logger.error(f"User not found: {subscription.customer}")
+                return
 
-        SubscriptionModel.create(
-            id=subscription.id,
-            created_at=subscription.created,
-            status=subscription.status,
-            current_period_start=subscription.current_period_start,
-            current_period_end=subscription.current_period_end,
-            cancel_at_period_end=subscription.cancel_at_period_end,
-            cancel_at=subscription.cancel_at,
-            user_id=user.id,
-        )
-
-    @classmethod
-    def update_subscription(cls, subscription: SubscriptionSchema):
-
-        subscription_model = SubscriptionModel.where(id__exact=subscription.id,).update(
-            values=dict(
+            subscription_model = SubscriptionModel(
+                id=subscription.id,
+                created_at=subscription.created,
                 status=subscription.status,
                 current_period_start=subscription.current_period_start,
                 current_period_end=subscription.current_period_end,
                 cancel_at_period_end=subscription.cancel_at_period_end,
                 cancel_at=subscription.cancel_at,
-            ),
-        )
-        if not subscription_model:
-            logger.error(f"Subscription not found: {subscription.id}")
-            cls.create_subscription(subscription)
+                user_id=user.id,
+            )
+            session.add(subscription_model)
+
+    @classmethod
+    def update_subscription(cls, subscription: SubscriptionSchema):
+        with session_scope() as session:
+            subscription_model = session.query(SubscriptionModel).filter(
+                SubscriptionModel.id == subscription.id
+            )
+            if not subscription_model:
+                logger.error(f"Subscription not found: {subscription.id}")
+                return
+            subscription_model.update(
+                dict(
+                    status=subscription.status,
+                    current_period_start=subscription.current_period_start,
+                    current_period_end=subscription.current_period_end,
+                    cancel_at_period_end=subscription.cancel_at_period_end,
+                    cancel_at=subscription.cancel_at,
+                )
+            )
